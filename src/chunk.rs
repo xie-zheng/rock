654a65d4fc737189db0a0a9621cd3eb2147bb7fc
xiezheng <xiezheng.dev@outlook.com>
2023-12-13 11:25:16 +0800

    add compressed line number

--- src/chunk.rs
+++ src/chunk.rs
@@ -19,7 +19,9 @@ type Value = f64;
 
 pub struct Chunk {
     code: Vec<u8>,
-    lines: Vec<usize>,
+    /// lines的编码，lines[i]=截止到第i行的所有指令数量
+    /// 这样在确定是第几行时只需二分查找一下
+    pub lines: Vec<usize>,
     constants: Vec<Value>,
 }
 
@@ -31,14 +33,35 @@ impl Chunk {
             constants: Vec::new(),
         }
     }
-    pub fn write(&mut self, op: OpCode) {
-        self.code.push(op as u8);
+
+    fn line_add(&mut self, line: usize, count: usize) {
+        if line == self.lines.len() {
+            self.lines.push(0);
+        }
+        if line > 0 && self.lines[line] == 0 {
+            self.lines[line] += self.lines[line - 1]
+        }
+        self.lines[line] += count;
     }
 
-    pub fn write_const(&mut self, op: OpCode, data: Value) {
+    fn get_line(&self, offset: usize) -> usize {
+        let offset = offset + 1;
+        match self.lines.binary_search(&offset) {
+            Ok(line) => line,
+            Err(line) => line,
+        }
+    }
+
+    pub fn write(&mut self, op: OpCode, line: usize) {
+        self.code.push(op as u8);
+        self.line_add(line, 1);
+    }
+
+    pub fn write_const(&mut self, op: OpCode, data: Value, line: usize) {
         self.constants.push(data);
         self.code
             .extend_from_slice(&[op as u8, self.constants.len() as u8 - 1]);
+        self.line_add(line, 2);
     }
 
     pub fn disassemble(&self, name: &str) {
@@ -50,9 +73,9 @@ impl Chunk {
         }
     }
 
-
     fn disassemble_instruction(&self, offset: usize) -> usize {
         print!("{:0>4} ", offset);
+        print!("line:{:0>4} ", self.get_line(offset));
 
         let instruction: OpCode = self.code[offset].into();
         match instruction {
@@ -71,7 +94,7 @@ impl Chunk {
     }
 
     fn const_instruction(&self, op: &str, offset: usize) -> usize {
-        println!("{} {}", op, self.constants[self.code[offset+1] as usize]);
+        println!("{} {}", op, self.constants[self.code[offset + 1] as usize]);
         offset + 2
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use rock::chunk::{Chunk, OpCode::*};
 fn main() {
     let mut chunk = Chunk::new();
 
-    chunk.write_const(CONSTANT, 1.2);
-    chunk.write(RETURN);
+    chunk.write_const(CONSTANT, 1.2, 0);
+    chunk.write(RETURN, 1);
 
     chunk.disassemble("test chunk");
+    println!("{:?}", chunk.lines);
 }
